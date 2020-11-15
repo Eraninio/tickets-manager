5fa1f3cdaffefedc8a553b3788155cd717d596f0
Eraninio <67857483+Eraninio@users.noreply.github.com>
2020-11-15 20:36:37 +0200

    first issue

--- client/src/components/SearchText.js
+++ client/src/components/SearchText.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './SearchText.css';
 
 const SearchText = (props) => (
-  <input id="searchInput" type="text" placeholder="Search Ticket..." onChange={(e) => (props.setSearchText(e.target.value))} />
+  <input id="searchInput" type="text" placeholder="Search Ticket..." onChange={(e) => (props.setSearchText((e.target.value).toLowerCase()))} />
 );
 
 export default SearchText;
